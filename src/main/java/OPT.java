48e0012fddcfe236b28a1eacba61280f6ebc1c0d
Yen Ton <yenton@yens-air.lan>
2023-06-11 00:34:17 -0700

    update

--- src/main/java/OPT.java
+++ src/main/java/OPT.java
@@ -2,47 +2,69 @@ import java.util.*;
 
 class OPT extends ReplacementAlgorithm {
     private List<Integer> pageList;
+    private int[] referenceString;
+    private int currentIndex;
 
-    public OPT(int pageFrameCount) {
+    public OPT(int pageFrameCount, int[] referenceString) {
         super(pageFrameCount);
-        this.pageList = new ArrayList<>(pageFrameCount);
+        this.pageList = new ArrayList<>();
+        this.referenceString = referenceString;
+        this.currentIndex = 0;
     }
 
     public void insert(int pageNumber) {
         if (!pageList.contains(pageNumber)) {
             pageFaultCount++;
-            if (pageList.size() >= pageFrameCount) {
-                int index = findOptimalIndex(pageList);
-                pageList.set(index, pageNumber);
-            } else {
+            if (pageList.size() < pageFrameCount) {
                 pageList.add(pageNumber);
+            } else {
+                int pageToRemove = getOptimalPage(pageNumber);
+                int index = pageList.indexOf(pageToRemove);
+                pageList.set(index, pageNumber);
             }
         }
+        currentIndex++;
     }
 
-    private int findOptimalIndex(List<Integer> pageList) {
-        int index = -1;
-        int farthestPage = Integer.MIN_VALUE;
+    private int getOptimalPage(int pageNumber) {
+        int indexToRemove = -1;
+        int farthestDistance = -1;
+
         for (int i = 0; i < pageList.size(); i++) {
             int currentPage = pageList.get(i);
-            int nextPageIndex = findNextAppearanceIndex(currentPage);
-            if (nextPageIndex == -1) {
-                return i;
+            if (!isPageInFuture(pageNumber, currentIndex, currentPage)) {
+                return currentPage;
             }
-            if (nextPageIndex > farthestPage) {
-                farthestPage = nextPageIndex;
-                index = i;
+            int distance = getDistanceToNextOccurrence(pageNumber, currentIndex);
+            if (distance > farthestDistance) {
+                farthestDistance = distance;
+                indexToRemove = i;
             }
         }
-        return index;
+
+        return pageList.get(indexToRemove);
     }
 
-    private int findNextAppearanceIndex(int pageNumber) {
-        for (int i = pageFaultCount; i < pageList.size(); i++) {
-            if (pageList.get(i) == pageNumber) {
-                return i;
+    private boolean isPageInFuture(int pageNumber, int currentIndex, int currentPage) {
+        for (int i = currentIndex + 1; i < referenceString.length; i++) {
+            if (referenceString[i] == currentPage) {
+                return true;
+            }
+            if (referenceString[i] == pageNumber) {
+                return false;
             }
         }
-        return -1;
+        return false;
+    }
+
+    private int getDistanceToNextOccurrence(int pageNumber, int currentIndex) {
+        int distance = 0;
+        for (int i = currentIndex + 1; i < referenceString.length; i++) {
+            distance++;
+            if (referenceString[i] == pageNumber) {
+                return distance;
+            }
+        }
+        return Integer.MAX_VALUE;
     }
 }
